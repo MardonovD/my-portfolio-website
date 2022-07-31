db375239262effe50f8b9ef2fbab98837e190141
Mardonov Dostonbek <mardonovdoston69@gmail.com>
2022-07-31 06:46:32 +0300

    main page create

--- src/componnets/header/Header.js
+++ src/componnets/header/Header.js
@@ -1,7 +1,15 @@
 import React from "react";
 import "./header.css";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container header-container">
+        <h5>Hello I'm</h5>
+        <h1>Mardonov Doston</h1>
+        <h5 className="text-light">Frontend Developer</h5>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
