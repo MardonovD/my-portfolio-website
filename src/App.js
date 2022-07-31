31a5c124248240294c08bf6ce3f4f167ccac389e
Mardonov Dostonbek <mardonovdoston69@gmail.com>
2022-07-31 05:54:00 +0300

    nav call

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
+import About from "./componnets/about/About";
+import Contact from "./componnets/contact/Contact";
+import Experience from "./componnets/experience/Experience";
+import Footer from "./componnets/footer/Footer";
 import Header from "./componnets/header/Header";
 import Nav from "./componnets/nav/Nav";
+import Portfolio from "./componnets/portfolio/Portfolio";
+import Services from "./componnets/services/Services";
+import Testimonials from "./componnets/testimonials/Testimonials";
 const App = () => {
   return (
     <>
       <Header />
       <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
     </>
   );
 };
